143899ac2fe4344139469c641c9f8cd797d723e8
20141105073 <798845992@qq.com>
2015-11-11 21:10:48 +0800

    Initial Commit

---
+++ q/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  q
+//
+//  Created by 20141105073 on 15/11/11.
+//  Copyright © 2015年 20141105073. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
